888a724910e860a3c8d2e6e8578f3a87293caaf5
Alexander <al.stepygin@gmail.com>
2025-10-15 08:37:33 +0300

    feat: добавлен код верификации Яндекс Вебмастера и счетчик Яндекс.Метрики

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Jost } from 'next/font/google'
 import './globals.css'
 import SessionProvider from '@/components/SessionProvider'
+import Script from 'next/script'
 
 const jost = Jost({ 
   subsets: ['latin', 'cyrillic'],
@@ -83,8 +84,11 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    yandex: 'your-yandex-verification-code',
+    yandex: '02e0d8beb8e2c52a',
     google: 'your-google-verification-code',
+  },
+  other: {
+    'yandex-verification': '02e0d8beb8e2c52a',
   },
 }
 
@@ -96,6 +100,34 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={jost.className}>
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104644843', 'ym');
+
+              ym(104644843, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/104644843" 
+              style={{ position: 'absolute', left: '-9999px' }} 
+              alt="" 
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+        
         <SessionProvider>
           {children}
         </SessionProvider>
